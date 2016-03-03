4530a5be35ac710df9d91e4c04371ffeb5824e00
Michael Gaare <michael.gaare@gmail.com>
2016-03-03 00:46:22 -0500

    we don't need the mode flags

--- src/cube2_extinfo/extinfo.clj
+++ src/cube2_extinfo/extinfo.clj
@@ -1,62 +1,6 @@
 (ns cube2-extinfo.extinfo
   (:require [cube2-extinfo.deserialise :as d]))
 
-(def mode-flags
-  {:m-team (bit-shift-left 1 0)
-   :m-noitems (bit-shift-left 1 1)
-   :m-noammo (bit-shift-left 1 2)
-   :m-insta (bit-shift-left 1 3)
-   :m-efficiency (bit-shift-left 1 4)
-   :m-tactics (bit-shift-left 1 5)
-   :m-capture (bit-shift-left 1 6)
-   :m-regen (bit-shift-left 1 7)
-   :m-ctf (bit-shift-left 1 8)
-   :m-protect (bit-shift-left 1 9)
-   :m-hold (bit-shift-left 1 10)
-   :m-overtime (bit-shift-left 1 11)
-   :m-edit (bit-shift-left 1 12)
-   :m-demo (bit-shift-left 1 13)
-   :m-local (bit-shift-left 1 14)
-   :m-lobby (bit-shift-left 1 15)
-   :m-dmsp (bit-shift-left 1 16)
-   :m-classicsp (bit-shift-left 1 17)
-   :m-slowmo (bit-shift-left 1 18)
-   :m-collect (bit-shift-left 1 19)})
-
-(defn flags
-  [& flags]
-  (let [m-flags (map mode-flags flags)]
-    (if (> (count flags) 1)
-      (apply bit-or m-flags)
-      (first m-flags))))
-
-(def modes
-  {:sp (flags :m-local :m-classicsp)
-   :dmsp (flags :m-local :m-dmsp)
-   :demo (flags :m-demo :m-local)
-   :ffa (flags :m-lobby)
-   :coop-edit (flags :m-edit)
-   :teamplay (flags :m-team)
-   :instagib (flags :m-noitems :m-insta)
-   :insta-team (flags :m-noitems :m-insta :m-team)
-   :efficiency (flags :m-noitems :m-efficiency)
-   :effic-team (flags :m-noitems :m-efficiency :m-team)
-   :tactics (flags :m-noitems :m-tactics)
-   :tac-team (flags :m-noitems :m-tactics :m-team)
-   :capture (flags :m-noammo :m-tactics :m-capture :m-team)
-   :regen-capture (flags :m-noitems :m-capture :m-regen :m-team)
-   :ctf (flags :m-ctf :m-team)
-   :insta-ctf (flags :m-noitems :m-insta :m-ctf :m-team)
-   :protect (flags :m-ctf :m-protect :m-team)
-   :insta-protect (flags :m-noitems :m-insta :m-ctf :m-protect :m-team)
-   :hold (flags :m-ctf :m-hold :m-team)
-   :insta-hold (flags :m-noitems :m-insta :m-ctf :m-hold :m-team)
-   :effic-ctf (flags :m-noitems :m-efficiency :m-ctf :m-team)
-   :effic-protect (flags :m-noitems :m-efficiency :m-ctf :m-protect :m-team)
-   :effic-hold (flags :m-noitems :m-efficiency :m-ctf :m-hold :m-team)
-   :collect (flags :m-collect :m-team)
-   :insta-collect (flags :m-noitems :m-insta :m-collect :m-team)
-   :effic-collect (flags :m-noitems :m-efficiency :m-collect :m-team)})
 
 
 ;; serverinfo
